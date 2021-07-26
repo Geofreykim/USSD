9921a0507c10281b5a367c2f002833fd2e9bc263
Geofreykim <geofkim98@gmail.com>
2021-07-26 15:29:17 +0300

    lol

--- Infinty charity tickets/db.php
+++ Infinty charity tickets/db.php
@@ -1,16 +1,16 @@
 <?php 
 
- $dbconnection = new mysqli("localhost","root","3!Bw_c!aHup]J-En","charity_tickets");
+  $dbconnection = new mysqli("localhost","root","3!Bw_c!aHup]J-En","charity_tickets");
+ 
 
- $dbconnection->query("CREATE DATABASE IF NOT EXISTS charity_tickets");
-
-
-
-$dbconnection->query("INSERT INTO tickets (id, name, price) VALUES  ('101', 'VIP', '1000')");
-
-$dbconnection->query("INSERT INTO tickets (id, name, price) VALUES  ('102', 'ORDINARY', '500')");
+ // $dbconnection->query("CREATE DATABASE IF NOT EXISTS charity_tickets");
   
- $dbconnection->query("CREATE TABLE IF NOT EXISTS tickets(id INT(10) NOT NULL AUTO_INCREMENT,PRIMARY KEY(id),name VARCHAR(20) NOT NULL, price DOUBLE (10,2) )");
+ $dbconnection->query("CREATE TABLE IF NOT EXISTS tickets(id INT(10) NOT NULL AUTO_INCREMENT,PRIMARY KEY(id),name VARCHAR(20) NOT NULL, price DOUBLE(10,2) )");
+
+
+$dbconnection->query("INSERT INTO tickets (id, name, price) VALUES('101', 'VIP', '1000')");
+
+$dbconnection->query("INSERT INTO tickets (id, name, price) VALUES('102', 'ORDINARY', '500')");
 
 
  
