9feca052f7d38cbd796b3f785cd4c07554e1e30f
Geofreykim <geofkim98@gmail.com>
2021-08-12 00:50:15 +0300

    new

--- Infinty charity tickets/db.php
+++ Infinty charity tickets/db.php
@@ -4,7 +4,7 @@
 
  
 
- // $dbconnection->query("CREATE DATABASE IF NOT EXISTS charity_tickets");
+ ////// $dbconnection->query("CREATE DATABASE IF NOT EXISTS charity_tickets");
   
  $dbconnection->query("CREATE TABLE IF NOT EXISTS tickets(id INT(10) NOT NULL AUTO_INCREMENT,PRIMARY KEY(id),name VARCHAR(20) NOT NULL, price DOUBLE(10,2) )");
 
